314d0dc87b866f2c3048de2792db03e600dde1fe
Forairaaaaa <applesyqd@outlook.com>
2024-06-18 10:19:28 +0800

    change to m5gfx auto

--- main/hal/display/hal_display.hpp
+++ main/hal/display/hal_display.hpp
@@ -1,84 +1,80 @@
-/**
- * @file hal_display.hpp
- * @author Forairaaaaa
- * @brief 
- * @version 0.1
- * @date 2023-06-28
- * 
- * @copyright Copyright (c) 2023
- * 
- */
-#pragma once
-// #include <M5GFX.h>
-// #include <lgfx/v1/panel/Panel_ST7789.hpp>
+// /**
+//  * @file hal_display.hpp
+//  * @author Forairaaaaa
+//  * @brief
+//  * @version 0.1
+//  * @date 2023-06-28
+//  *
+//  * @copyright Copyright (c) 2023
+//  *
+//  */
+// #pragma once
+// // #include <M5GFX.h>
+// // #include <lgfx/v1/panel/Panel_ST7789.hpp>
 
-#define LGFX_USE_V1
-#include <LovyanGFX.hpp>
+// #define LGFX_USE_V1
+// #include <LovyanGFX.hpp>
 
+// #define LCD_MOSI_PIN 35
+// #define LCD_MISO_PIN -1
+// #define LCD_SCLK_PIN 36
+// #define LCD_DC_PIN   34
+// #define LCD_CS_PIN   37
+// #define LCD_RST_PIN  33
+// #define LCD_BUSY_PIN -1
+// #define LCD_BL_PIN   38
 
+// class LGFX_Cardputer : public lgfx::LGFX_Device {
+//     lgfx::Panel_ST7789 _panel_instance;
+//     lgfx::Bus_SPI _bus_instance;
+//     lgfx::Light_PWM _light_instance;
 
-#define LCD_MOSI_PIN 35
-#define LCD_MISO_PIN -1
-#define LCD_SCLK_PIN 36
-#define LCD_DC_PIN   34
-#define LCD_CS_PIN   37
-#define LCD_RST_PIN  33
-#define LCD_BUSY_PIN -1
-#define LCD_BL_PIN   38
+//    public:
+//     LGFX_Cardputer(void) {
+//         {
+//             auto cfg = _bus_instance.config();
 
+//             cfg.pin_mosi   = LCD_MOSI_PIN;
+//             cfg.pin_miso   = LCD_MISO_PIN;
+//             cfg.pin_sclk   = LCD_SCLK_PIN;
+//             cfg.pin_dc     = LCD_DC_PIN;
+//             cfg.freq_write = 40000000;
 
-class LGFX_Cardputer : public lgfx::LGFX_Device {
-    lgfx::Panel_ST7789 _panel_instance;
-    lgfx::Bus_SPI _bus_instance;
-    lgfx::Light_PWM _light_instance;
+//             _bus_instance.config(cfg);
+//             _panel_instance.setBus(&_bus_instance);
+//         }
+//         {
+//             auto cfg = _panel_instance.config();
 
-   public:
-    LGFX_Cardputer(void) {
-        {
-            auto cfg = _bus_instance.config();
+//             cfg.invert       = true;
+//             cfg.pin_cs       = LCD_CS_PIN;
+//             cfg.pin_rst      = LCD_RST_PIN;
+//             cfg.pin_busy     = LCD_BUSY_PIN;
+//             // cfg.panel_width  = 240;
+//             // cfg.panel_height = 240;
+//             // cfg.offset_x     = 52;
+//             // cfg.offset_y     = 40;
+//             cfg.panel_width  = 135;
+//             cfg.panel_height = 240;
+//             cfg.offset_x     = 52;
+//             cfg.offset_y     = 40;
 
-            cfg.pin_mosi   = LCD_MOSI_PIN;
-            cfg.pin_miso   = LCD_MISO_PIN;
-            cfg.pin_sclk   = LCD_SCLK_PIN;
-            cfg.pin_dc     = LCD_DC_PIN;
-            cfg.freq_write = 40000000;
+//             _panel_instance.config(cfg);
+//         }
+//         {   // バックライト制御の設定を行います。（必要なければ削除）
+//             auto cfg = _light_instance.config();    // バックライト設定用の構造体を取得します。
 
-            _bus_instance.config(cfg);
-            _panel_instance.setBus(&_bus_instance);
-        }
-        {
-            auto cfg = _panel_instance.config();
+//             cfg.pin_bl = LCD_BL_PIN;              // バックライトが接続されているピン番号
+//             cfg.invert = false;           // バックライトの輝度を反転させる場合 true
+//             // cfg.freq   = 44100;           // バックライトのPWM周波数
+//             cfg.freq   = 200;           // バックライトのPWM周波数
+//             // cfg.freq   = 500;           // バックライトのPWM周波数
+//             cfg.pwm_channel = 7;          // 使用するPWMのチャンネル番号
 
-            cfg.invert       = true;
-            cfg.pin_cs       = LCD_CS_PIN;
-            cfg.pin_rst      = LCD_RST_PIN;
-            cfg.pin_busy     = LCD_BUSY_PIN;
-            // cfg.panel_width  = 240;
-            // cfg.panel_height = 240;
-            // cfg.offset_x     = 52;
-            // cfg.offset_y     = 40;
-            cfg.panel_width  = 135;
-            cfg.panel_height = 240;
-            cfg.offset_x     = 52;
-            cfg.offset_y     = 40;
+//             _light_instance.config(cfg);
+//             _panel_instance.setLight(&_light_instance);  // バックライトをパネルにセットします。
+//         }
 
-            _panel_instance.config(cfg);
-        }
-        {   // バックライト制御の設定を行います。（必要なければ削除）
-            auto cfg = _light_instance.config();    // バックライト設定用の構造体を取得します。
-
-            cfg.pin_bl = LCD_BL_PIN;              // バックライトが接続されているピン番号
-            cfg.invert = false;           // バックライトの輝度を反転させる場合 true
-            // cfg.freq   = 44100;           // バックライトのPWM周波数
-            cfg.freq   = 200;           // バックライトのPWM周波数
-            // cfg.freq   = 500;           // バックライトのPWM周波数
-            cfg.pwm_channel = 7;          // 使用するPWMのチャンネル番号
-
-            _light_instance.config(cfg);
-            _panel_instance.setLight(&_light_instance);  // バックライトをパネルにセットします。
-        }
-            
-        setPanel(&_panel_instance);
-    }
-};
-
+//         setPanel(&_panel_instance);
+//     }
+// };
--- main/hal/hal.h
+++ main/hal/hal.h
@@ -9,7 +9,7 @@
  *
  */
 #pragma once
-#include "../../components/LovyanGFX/src/LovyanGFX.hpp"
+#include "../../components/M5GFX/src/M5GFX.h"
 #include "keyboard/keyboard.h"
 #include "mic/Mic_Class.hpp"
 #include "speaker/Speaker_Class.hpp"
@@ -21,68 +21,60 @@
 namespace HAL
 {
     /**
-    * @brief Hal base for DI
-    *
-    */
-    class Hal 
+     * @brief Hal base for DI
+     *
+     */
+    class Hal
     {
-        protected:
-            LGFX_Device* _display;
-            LGFX_Sprite* _canvas;
-            LGFX_Sprite* _canvas_system_bar;
-            LGFX_Sprite* _canvas_keyboard_bar;
+    protected:
+        LGFX_Device* _display;
+        LGFX_Sprite* _canvas;
+        LGFX_Sprite* _canvas_system_bar;
+        LGFX_Sprite* _canvas_keyboard_bar;
 
-            KEYBOARD::Keyboard* _keyboard;
-            m5::Mic_Class* _mic;
-            m5::Speaker_Class* _speaker;
-            Button* _homeButton;
-            SDCard* _sdcard;
+        KEYBOARD::Keyboard* _keyboard;
+        m5::Mic_Class* _mic;
+        m5::Speaker_Class* _speaker;
+        Button* _homeButton;
+        SDCard* _sdcard;
 
-            bool _sntp_adjusted;
+        bool _sntp_adjusted;
 
-        public:
-            Hal() : 
-            _display(nullptr), 
-            _canvas(nullptr), 
-            _canvas_system_bar(nullptr),
-            _canvas_keyboard_bar(nullptr),
-            _keyboard(nullptr),
-            _mic(nullptr),
-            _speaker(nullptr),
-            _homeButton(nullptr),
-            _sdcard(nullptr),
-            _sntp_adjusted(false)
-            {}
+    public:
+        Hal()
+            : _display(nullptr), _canvas(nullptr), _canvas_system_bar(nullptr), _canvas_keyboard_bar(nullptr),
+              _keyboard(nullptr), _mic(nullptr), _speaker(nullptr), _homeButton(nullptr), _sdcard(nullptr),
+              _sntp_adjusted(false)
+        {
+        }
 
-            // Getter 
-            inline LGFX_Device* display() { return _display; }
-            inline LGFX_Sprite* canvas() { return _canvas; }
-            inline LGFX_Sprite* canvas_system_bar() { return _canvas_system_bar; }
-            inline LGFX_Sprite* canvas_keyboard_bar() { return _canvas_keyboard_bar; }
-            inline KEYBOARD::Keyboard* keyboard() { return _keyboard; }
-            inline m5::Mic_Class* mic() { return _mic; }
-            inline m5::Speaker_Class* Speaker() { return _speaker; }
-            inline Button* homeButton() { return _homeButton; }
-            inline SDCard* sdcard() { return _sdcard; }
+        // Getter
+        inline LGFX_Device* display() { return _display; }
+        inline LGFX_Sprite* canvas() { return _canvas; }
+        inline LGFX_Sprite* canvas_system_bar() { return _canvas_system_bar; }
+        inline LGFX_Sprite* canvas_keyboard_bar() { return _canvas_keyboard_bar; }
+        inline KEYBOARD::Keyboard* keyboard() { return _keyboard; }
+        inline m5::Mic_Class* mic() { return _mic; }
+        inline m5::Speaker_Class* Speaker() { return _speaker; }
+        inline Button* homeButton() { return _homeButton; }
+        inline SDCard* sdcard() { return _sdcard; }
 
-            inline void setSntpAdjusted(bool isAdjusted) { _sntp_adjusted = isAdjusted; }
-            inline bool isSntpAdjusted(void) { return _sntp_adjusted; }
+        inline void setSntpAdjusted(bool isAdjusted) { _sntp_adjusted = isAdjusted; }
+        inline bool isSntpAdjusted(void) { return _sntp_adjusted; }
 
+        // Canvas
+        inline void canvas_system_bar_update() { _canvas_system_bar->pushSprite(_canvas_keyboard_bar->width(), 0); }
+        inline void canvas_keyboard_bar_update() { _canvas_keyboard_bar->pushSprite(0, 0); }
+        inline void canvas_update() { _canvas->pushSprite(_canvas_keyboard_bar->width(), _canvas_system_bar->height()); }
 
-            
-            // Canvas 
-            inline void canvas_system_bar_update() { _canvas_system_bar->pushSprite(_canvas_keyboard_bar->width(), 0); }
-            inline void canvas_keyboard_bar_update() { _canvas_keyboard_bar->pushSprite(0, 0); }
-            inline void canvas_update() { _canvas->pushSprite(_canvas_keyboard_bar->width(), _canvas_system_bar->height()); }
+        // Override
+        virtual std::string type() { return "null"; }
+        virtual void init() {}
 
-            // Override 
-            virtual std::string type() { return "null"; }
-            virtual void init() {}
+        virtual void playLastSound() {}
+        virtual void playNextSound() {}
+        virtual void playKeyboardSound() {}
 
-            virtual void playLastSound() {}
-            virtual void playNextSound() {}
-            virtual void playKeyboardSound() {}
-
-            virtual uint8_t getBatLevel() { return 100; }
+        virtual uint8_t getBatLevel() { return 100; }
     };
-}
+} // namespace HAL
